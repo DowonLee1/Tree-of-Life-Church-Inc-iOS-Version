747bf01942546be02810e889d26d13d0319a454d
Dowon <oplolpz@gmail.com>
2020-05-13 00:10:04 -0400

    Initial Commit

---
+++ TTOLC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTOLC
+//
+//  Created by Dowon on 5/13/20.
+//  Copyright © 2020 Dowon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
