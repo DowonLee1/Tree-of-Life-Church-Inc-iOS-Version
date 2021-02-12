ac526c6d5e072dbf33516247e7a2a2c9a4dbf9fd
Dowon Lee <oplolpz@gmail.com>
2021-02-12 00:55:52 -0500

    Update DetailBookViewController.swift
    Framework and better response for share button

--- TTOLC/DetailBookViewController.swift
+++ TTOLC/DetailBookViewController.swift
@@ -15,7 +15,11 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var shareButtonImage: UIImageView!
+    
     @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var languageSegment: UISegmentedControl!
     @IBOutlet weak var buyingButton: UIButton!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
@@ -23,6 +27,8 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
     var passedIndex = 0
     
     var passedViewController = ""
+    
+    let screenSize = UIScreen.main.bounds
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,7 +73,28 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
         image.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         image.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         image.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
-        image.heightAnchor.constraint(equalToConstant: image.frame.size.width / 1.38).isActive = true
+        image.heightAnchor.constraint(equalToConstant: image.frame.size.width / 1.6).isActive = true
+        
+        
+        
+        
+        if screenSize.height <= screenSize.width * 2{
+            print("screen ratio is 16:9 as iphone 8")
+            shareButtonImage.frame.size.height = 16
+            shareButtonImage.frame.size.width = 17
+        }
+        // if screen ratio is 21:9
+        else if screenSize.height >= screenSize.width * 2{
+            print("screen ratio is 21:9 as iphone x")
+            shareButtonImage.frame.size.height = 16
+            shareButtonImage.frame.size.width = 17
+        }
+        
+        // for better touch response, separate image and button then make button size bigger than image.
+        shareButton.center = shareButtonImage.center
+        shareButton.frame.size.height = 30
+        shareButton.frame.size.width = 30
+        
         
         contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.topAnchor.constraint(equalTo: image.bottomAnchor).isActive = true
@@ -75,21 +102,29 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
         contentView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         
-        
-        
-        
-//        detailLabel.translatesAutoresizingMaskIntoConstraints = false
-//        detailLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 30).isActive = true
-//        detailLabel.leftAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
-        
-//        buyingButton.frame.size.width = view.frame.size.width / 3.47
-//        buyingButton.frame.size.height = buyingButton.frame.size.height / 2.38
         buyingButton.layer.borderWidth = 1
         buyingButton.layer.borderColor = UIColor.black.cgColor
             
         contentView.layer.shadowOffset = CGSize(width: 0, height: 1)
         contentView.layer.shadowOpacity = 0.2
         contentView.layer.shadowColor = UIColor.black.cgColor
+        
+        descriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        descriptionTextView.leftAnchor.constraint(equalTo: descriptionLabel.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: descriptionLabel.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        
+        if (passedViewController == "ABOUT US") || (passedViewController == "HISTORY OF REDEMPTION") {
+            descriptionTextView.topAnchor.constraint(equalTo: languageSegment.bottomAnchor, constant: 10).isActive = true
+        }
+        else {
+            languageSegment.alpha = 0
+            descriptionTextView.topAnchor.constraint(equalTo: languageSegment.topAnchor).isActive = true
+        }
+        
+    }
+    @IBAction func didChangeSegment(_ sender: UISegmentedControl) {
     }
     
     @IBAction func shareButtonClicked(_ sender: UIButton) {
@@ -108,14 +143,20 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
     }
     
     private func sendEmail() {
-        let composeVC = MFMailComposeViewController()
-        composeVC.mailComposeDelegate = self
-        // Configure the fields of the interface.
-        composeVC.setToRecipients(["info.ttolc@gmail.com"])
-        composeVC.setSubject("Request Buying \(passedHorBooks[passedIndex].bookTitle)")
-        composeVC.setMessageBody("Hello Tree of life church, ", isHTML: false)
-        // Present the view controller modally.
-        self.present(composeVC, animated: true, completion: nil)
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["info.ttolc@gmail.com"])
+            mail.setSubject("Request for Buying \(passedHorBooks[passedIndex].bookTitle)")
+            mail.setMessageBody("Hello Tree of life church, ", isHTML: true)
+
+            present(mail, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Setup Your Email on Mail App", message: nil, preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
+            alert.view.tintColor = UIColor.black
+            present(alert, animated: true, completion: nil)
+        }
     }
 
     private func mailComposeController(controller: MFMailComposeViewController,
@@ -124,3 +165,5 @@ class DetailBookViewController: UIViewController, MFMailComposeViewControllerDel
     }
 
 }
+
+
